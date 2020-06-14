3dd6111ccc7205befce630c55248c5347f4c01d7
Sridaran Thoniyil <sri7thon@gmail.com>
2020-06-14 00:30:40 -0400

    Write code to print contents of cwd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+fn list_files_in_directory(directory: &std::path::PathBuf ) -> std::io::Result<std::fs::ReadDir> {
+    directory.as_path().read_dir()
+}
+
+fn main() -> std::io::Result<()> {
+    let cwd = env::current_dir()?;
+    let files = list_files_in_directory(&cwd)?;
+    for file in files {
+        if let Ok(file_entry) = file {
+            println!("{}", file_entry.path().file_name().unwrap().to_string_lossy().into_owned());
+        }
+        else {
+            println!("ERR");
+        }
+    }
+
+    Ok(())
 }
