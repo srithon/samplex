0ce48d6e3be248ecae46bbb9b0741fb83d62250d
Sridaran Thoniyil <sri7thon@gmail.com>
2020-07-01 12:08:03 -0400

    Make util functions determine files vs directories

--- src/util.rs
+++ src/util.rs
@@ -1,20 +1,33 @@
 use std::io::Result;
 use std::env;
 
-pub fn list_files_in_directory(directory: &std::path::PathBuf ) -> Result<std::fs::ReadDir> {
+pub enum FileType {
+    File(String),
+    Directory(String),
+}
+
+pub fn list_directory_contents(directory: &std::path::PathBuf ) -> Result<std::fs::ReadDir> {
     directory.as_path().read_dir()
 }
 
-pub fn list_filenames_in_current_directory() -> Result<Vec<String>> {
+pub fn list_current_directory_contents() -> Result<Vec<FileType>> {
     let cwd = env::current_dir()?;
-    let files = list_files_in_directory(&cwd)?;
+    let files = list_directory_contents(&cwd)?;
     Ok(files.filter_map(|file_entry| {
         if let Ok(entry) = file_entry {
-            Some(entry.path().file_name().unwrap().to_string_lossy().into_owned())
+            let contents = entry.path().file_name().unwrap().to_string_lossy().into_owned();
+            let entry_filetype = entry.file_type();
+            if let Ok(entry_filetype) = entry_filetype {
+                if entry_filetype.is_dir() {
+                return Some(FileType::Directory(contents));
+                }
+                else {
+                    return Some(FileType::File(contents));
+                }
+            }
         }
-        else {
-            None
-        }
+
+        return None;
     }).collect())
 }
 
