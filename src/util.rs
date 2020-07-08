db50701bb7b2f0b51f34b76e62cf3863144c9990
Sridaran Thoniyil <sri7thon@gmail.com>
2020-07-07 23:22:39 -0400

    Filter out files bigger than 40 MB

--- src/util.rs
+++ src/util.rs
@@ -16,13 +16,30 @@ pub fn list_current_directory_contents() -> Result<Vec<FileType>> {
     Ok(files.filter_map(|file_entry| {
         if let Ok(entry) = file_entry {
             let contents = entry.path().file_name().unwrap().to_string_lossy().into_owned();
-            let entry_filetype = entry.file_type();
-            if let Ok(entry_filetype) = entry_filetype {
-                if entry_filetype.is_dir() {
-                return Some(FileType::Directory(contents));
-                }
-                else {
-                    return Some(FileType::File(contents));
+
+            let metadata = entry.metadata();
+
+            if let Ok(metadata) = metadata {
+                // 40 MB in bytes
+                let MAX_ACCEPTABLE_FILE_LENGTH_BYTES = 40 * 1024 * 1024;
+
+                let file_length = metadata.len();
+                let entry_filetype = entry.file_type();
+
+                if let Ok(entry_filetype) = entry_filetype {
+                    if entry_filetype.is_dir() {
+                        return Some(FileType::Directory(contents));
+                    }
+                    else {
+                        // 40 MB in bytes
+                        if file_length < MAX_ACCEPTABLE_FILE_LENGTH_BYTES {
+                            println!("File length {} passed check", file_length as f64 / 1024 as f64 / 1024.0);
+                            return Some(FileType::File(contents));
+                        }
+                        else {
+                            println!("File length {} failed check", file_length as f64 / 1024 as f64 / 1024.0);
+                        }
+                    }
                 }
             }
         }
